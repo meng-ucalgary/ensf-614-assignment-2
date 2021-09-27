97a585384ffcbc58e1cc9afc4bf5f95fb99eac43
Bhavyai Gupta <zbhavyai@gmail.com>
2021-09-27 02:50:23 -0600

    Added function definitions

--- lab2exe_E.c
+++ lab2exe_E.c
@@ -10,12 +10,29 @@
 
 #include "lab2exe_E.h"
 
-struct cplx
-cplx_add(struct cplx z1, struct cplx z2)
+struct cplx cplx_add(struct cplx z1, struct cplx z2)
 {
     struct cplx result;
 
     result.real = z1.real + z2.real;
     result.imag = z1.imag + z2.imag;
+
     return result;
+}
+
+void cplx_subtract(struct cplx z1, struct cplx z2, struct cplx *difference)
+{
+    (*difference).real = z1.real - z2.real;
+    (*difference).imag = z1.imag - z2.imag;
+}
+
+void cplx_multiply(const struct cplx *pz1, const struct cplx *pz2, struct cplx *product)
+{
+    double a = (*pz1).real;
+    double b = (*pz1).imag;
+    double c = (*pz2).real;
+    double d = (*pz2).imag;
+
+    (*product).real = (a*c - b*d);
+    (*product).imag = (a*d + b*c);
 }
